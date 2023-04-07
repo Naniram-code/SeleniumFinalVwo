6d8dfda8a95bf34474f0728ae596761bd52f21f3
Naniram-code <NaniramBhujel@gmail.com>
2023-04-06 21:20:21 -0400

    add listener code

--- src/main/java/page/web/WaitForHelper.java
+++ src/main/java/page/web/WaitForHelper.java
@@ -17,6 +17,7 @@ public class WaitForHelper {
     public WaitForHelper(WebDriver driver ){this.driver =driver;}
 
     public void implicitWait(){driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS);}
+
     public WebElement presenceOfElement(final By elementLocation){
         return new WebDriverWait(driver,Duration.ofSeconds(20)).
                 until(ExpectedConditions.presenceOfElementLocated(elementLocation));
